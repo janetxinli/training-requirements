431db44a0cfa65d4c59eb8051b38f1c8249c7ddc
janetxinli <janetxinli@gmail.com>
2021-06-23 16:00:14 -0700

    slightly change chart font sizes

--- Front-End Web Development/src/helpers/getFontSize.js
+++ Front-End Web Development/src/helpers/getFontSize.js
@@ -10,13 +10,18 @@ export const getTitleFontSize = () => {
 
 export const getAxisFontSize = () => {
   if (window.innerWidth > 1280) {
+    return 28;
+  }
+  if (window.innerWidth > 720) {
     return 24;
   }
-
   return 16;
 };
 
 export const getDatalabelFontSize = () => {
+  if (window.innerWidth > 1280) {
+    return 20;
+  }
   if (window.innerWidth > 720) {
     return 16;
   }
