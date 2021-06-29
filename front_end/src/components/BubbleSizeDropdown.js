b8331d8e3b691016d518b2c2753fe6f19615ad3d
janetxinli <janetxinli@gmail.com>
2021-06-29 13:50:50 -0700

    align variables, styling with AxisDropdown

--- front_end/src/components/BubbleSizeDropdown.js
+++ front_end/src/components/BubbleSizeDropdown.js
@@ -1,25 +1,25 @@
 import React from "react";
-import styles from "./BubbleSizeDropdown.module.scss";
 
 export const BubbleSizeDropdown = ({ sizeValue, setSizeValue, hoverValue }) => (
-  <select
-    name="pets"
-    defaultValue={sizeValue}
-    onChange={(e) => setSizeValue(e.target.value)}
-    className={styles.dropdown}
-  >
-    <option value="population">
-      {sizeValue === "population" && hoverValue ? hoverValue : "Population"}
-    </option>
-    <option value="babiesPerWoman">
-      {sizeValue === "babiesPerWoman" && hoverValue
-        ? hoverValue
-        : "Babies per Woman"}
-    </option>
-    <option value="co2">
-      {sizeValue === "co2" && hoverValue
-        ? hoverValue
-        : "CO2 Emissions per Person"}
-    </option>
-  </select>
+  <span className="select-container">
+    <p className="select-label">bubble size</p>
+    <select
+      defaultValue={sizeValue}
+      onChange={(e) => setSizeValue(e.target.value)}
+    >
+      <option value="population">
+        {sizeValue === "population" && hoverValue ? hoverValue : "Population"}
+      </option>
+      <option value="fertilityRate">
+        {sizeValue === "fertilityRate" && hoverValue
+          ? hoverValue
+          : "Babies per Woman"}
+      </option>
+      <option value="co2">
+        {sizeValue === "co2" && hoverValue
+          ? hoverValue
+          : "CO2 Emissions per Person"}
+      </option>
+    </select>
+  </span>
 );
